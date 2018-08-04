a1c1e872a7eb2eb09a96bb2c0bad8936ad4f15e5
Victor Norgren <victor@logotype.se>
2018-08-05 03:05:43 +0800

    migrate to gitlab

--- examples/example_md5.js
+++
@@ -1,6 +0,0 @@
-var HashDog, hashDog;
-HashDog = require('./../build/HashDog').HashDog;
-hashDog = new HashDog({hash: '2655dd21148f2433763d313407d5d820', type: 'MD5', chars: 'abcdefghijklmnopqrstuvwxyz0123456789'});
-hashDog.on('success', function(data) {
-    console.log(data);
-});
